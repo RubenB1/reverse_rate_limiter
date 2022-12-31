16acac90858a3eda9e163036c35e3831a359053d
RubenB1 <RubenB1@github.com>
2022-12-31 16:08:37 +0100

    comment out

--- rate_testing.py
+++ rate_testing.py
@@ -4,11 +4,11 @@ from reverse_api_rate_limiter import ReverseRateLimiter
 reverse_rate_limiter = ReverseRateLimiter(
     host = 'eu2-immense-cheetah-30534.upstash.io',
     port = 30534,
-    password = 'b9001de47f964351bc2bbf545fad3ff3')
+    password = '********')
 
 ## Simulate a setup where the rate limit is 5 requests per second per key.
 # define the key
-key = 'test_key'
+key = 'user_id'
 
 # define the window in seconds
 window_in_seconds = 1
@@ -16,17 +16,17 @@ window_in_seconds = 1
 # define the rate limit per window
 limit_per_window = 5
 
-# request a credit in a loop and print if the credit is granted each time (will print true or false)
-for i in range(100):
-    credit_is_granted = reverse_rate_limiter.get_api_request_credit(
-        key = key,
-        window_in_seconds = window_in_seconds,
-        limit_per_window = limit_per_window,
-        wait_seconds_if_credit_not_granted=0.1,
-        max_retries=2
-        )
-    
-    print(credit_is_granted)
+# # request a credit in a loop and print if the credit is granted each time (will print true or false)
+# for i in range(100):
+#     credit_is_granted = reverse_rate_limiter.get_api_request_credit(
+#         key = key,
+#         window_in_seconds = window_in_seconds,
+#         limit_per_window = limit_per_window,
+#         wait_seconds_if_credit_not_granted=0.1,
+#         max_retries=2
+#         )
+#     
+#     print(credit_is_granted)
 
 # request a credit in a loop and print if the credit is granted each time
 for i in range(100):
